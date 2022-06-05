2f081742d35a85da580d8df66e5a195013b02f17
samsvk <samsvk00@gmail.com>
2022-06-05 23:25:16 +0100

    updated

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,12 @@ export default function Home() {
       <Head>
         <title>Viperino</title>
       </Head>
+
+      <div className="flex flex-col items-center justify-center w-full h-screen">
+        <div className="p-6 rounded bg-slate-800">
+          <h1 className="text-3xl">test.</h1>
+        </div>
+      </div>
     </div>
   );
 }
