3bc2ffbc9e69010ff52a994c35f2b501003fb522
samsvk <samsvk00@gmail.com>
2022-06-05 23:34:48 +0100

    updated styling

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
       </Head>
 
       <div className="flex flex-col items-center justify-center w-full h-screen">
-        <div className="p-6 border rounded border-neutral-700 bg-neutral-900 drop-shadow-md">
+        <div className="w-full max-w-screen-md p-6 border rounded border-zinc-700 bg-neutral-900 drop-shadow-md min-h-[300px]">
           <h1 className="text-3xl"></h1>
         </div>
       </div>
