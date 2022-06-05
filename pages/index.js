4f06cac50f9cb3676d08f7821325e921aa1ab7a3
samsvk <samsvk00@gmail.com>
2022-06-06 00:22:48 +0100

    updated

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,19 @@ export default function Home() {
       </Head>
 
       <div className="flex flex-col items-center justify-center w-full h-screen">
-        <div className="w-full max-w-screen-md p-6 border rounded border-zinc-700 bg-neutral-900 drop-shadow-md min-h-[300px]">
-          <h1 className="text-3xl"></h1>
+        <div className="w-full max-w-screen-lg border rounded border-gray-100 bg-white drop-shadow-md min-h-[450px] flex flex-row">
+          <div className="w-full bg-gray-100 max-w-fit">
+            <ul className="inline-block w-full px-3 py-2 justify-evenly">
+              <li className="inline-block w-3 h-3 mr-1.5 rounded-full max-w-3" style={{ background: "#f87171" }}></li>
+              <li className="inline-block w-3 h-3 mr-1.5 rounded-full max-w-3" style={{ background: "#facc14" }}></li>
+              <li className="inline-block w-3 h-3 mr-1.5 rounded-full max-w-3" style={{ background: "#49de80" }}></li>
+            </ul>
+            <div className="px-2 py-1">
+              <div className="px-3.5 rounded bg-gray-200">Svk's Index</div>
+              <div className="p-3.5">Tags</div>
+            </div>
+          </div>
+          <div className="w-full p-6 max-w-fit">1231231233</div>
         </div>
       </div>
     </div>
