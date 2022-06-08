9367e6bf92a6b678987b1a5f495c85e202dca475
samsvk <samsvk00@gmail.com>
2022-06-08 04:58:53 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,7 @@ export default function Home({ posts }) {
             <div className="px-4 py-2.5 bg-gray-200 rounded-md hover:bg-gray-400 duration-300 hover:cursor-pointer">
               <Image src="/folder.png" alt="folder picture" width="100px" height="100px" />
             </div>
-            <h1 className="m-0 mt-2 text-xs font-semibold text-gray-500">{post}</h1>
+            <h1 className="m-0 mt-2 text-xs font-semibold text-gray-500">{post.replace(/^./, (str) => str.toUpperCase())}</h1>
           </div>
         </div>
       ))}
