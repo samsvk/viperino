0489d78d9bfe743947ed658b454d4563f5453aa2
samsvk <samsvk00@gmail.com>
2022-06-08 05:01:50 +0100

    working readdir

--- pages/index.js
+++ pages/index.js
@@ -32,24 +32,28 @@ import path from "path";
 import fs from "fs";
 
 export default function Home({ posts }) {
+  console.log(posts);
   return (
     <>
-      {posts.map((post, index) => (
-        <div key={index} className="flex items-center justify-center w-full max-w-3xl min-h-screen p-10 mx-auto my-0">
-          <div className="flex flex-col items-center justify-center">
+      <div className="flex items-center justify-center w-full max-w-3xl min-h-screen gap-3 p-10 mx-auto my-0">
+        {posts.map((post, index) => (
+          <div key={index} className="flex flex-col items-center justify-center">
             <div className="px-4 py-2.5 bg-gray-200 rounded-md hover:bg-gray-400 duration-300 hover:cursor-pointer">
               <Image src="/folder.png" alt="folder picture" width="100px" height="100px" />
             </div>
             <h1 className="m-0 mt-2 text-xs font-semibold text-gray-500">{post.replace(/^./, (str) => str.toUpperCase())}</h1>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </>
   );
 }
 
 export async function getStaticProps() {
   const postDirectory = path.join(process.cwd(), "classes/");
-  const posts = await fs.readdirSync(postDirectory).filter((file) => file);
+  const posts = await fs.readdirSync(postDirectory).map((file) => {
+    console.log(file);
+    return file;
+  });
   return { props: { posts } };
 }
