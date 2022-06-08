9decadcf33b301281b4eb8b3f983ed651655758d
samsvk <samsvk00@gmail.com>
2022-06-08 21:23:45 +0100

    working routes

--- pages/index.js
+++ pages/index.js
@@ -30,17 +30,19 @@ import Articles from "../src/components/articles";
 import Image from "next/image";
 import path from "path";
 import fs from "fs";
+import Link from "next/link";
 
 export default function Home({ posts }) {
-  console.log(posts);
   return (
     <>
       <div className="flex items-center justify-center w-full max-w-3xl min-h-screen gap-3 p-10 mx-auto my-0">
         {posts.map((post, index) => (
           <div key={index} className="flex flex-col items-center justify-center">
-            <div className="px-4 py-2.5 bg-gray-200 rounded-md hover:bg-gray-400 duration-300 hover:cursor-pointer">
-              <Image src="/folder.png" alt="folder picture" width="100px" height="100px" />
-            </div>
+            <Link href={`${post}`}>
+              <div className="px-4 py-2.5 bg-gray-200 rounded-md hover:bg-gray-400 duration-300 hover:cursor-pointer">
+                <Image src="/folder.png" alt="folder picture" width="100px" height="100px" />
+              </div>
+            </Link>
             <h1 className="m-0 mt-2 text-xs font-semibold text-gray-500">{post.replace(/^./, (str) => str.toUpperCase())}</h1>
           </div>
         ))}
@@ -51,9 +53,6 @@ export default function Home({ posts }) {
 
 export async function getStaticProps() {
   const postDirectory = path.join(process.cwd(), "classes/");
-  const posts = await fs.readdirSync(postDirectory).map((file) => {
-    console.log(file);
-    return file;
-  });
+  const posts = await fs.readdirSync(postDirectory).map((file) => file);
   return { props: { posts } };
 }
---
+++ pages/viper.js
@@ -0,0 +1,11 @@
+export default function Viper() {
+  return (
+    <>
+      <div className="flex items-center justify-center w-full max-w-3xl min-h-screen gap-3 p-10 mx-auto my-0">welcome to viper</div>
+    </>
+  );
+}
+
+// export async function getStaticProps() {
+//   //   return { props: { posts } };
+// }
