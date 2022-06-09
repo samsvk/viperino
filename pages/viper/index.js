a29c84830f7bd6c27c0a6003b00d5dad0bfd5096
samsvk <samsvk00@gmail.com>
2022-06-09 04:47:10 +0100

    working list

--- pages/viper/index.js
+++ pages/viper/index.js
@@ -1,4 +1,7 @@
-export default function Viper() {
+import fs from "fs";
+import path from "path";
+
+export default function Viper({ posts }) {
   return (
     <>
       <div className="flex items-center justify-center w-full max-w-3xl min-h-screen gap-3 p-10 mx-auto my-0">welcome to viper</div>
@@ -6,6 +9,14 @@ export default function Viper() {
   );
 }
 
-// export async function getStaticProps() {
-//   //   return { props: { posts } };
-// }
+export async function getStaticProps() {
+  const paths = path.join(process.cwd(), `classes/viper`);
+  const posts = await fs.readdirSync(paths).map((path) => {
+    const parts = path.split("/");
+    const fileName = parts[parts.length - 1];
+    const [slug, _ext] = fileName.split(".");
+    return slug;
+  });
+
+  return { props: { posts } };
+}
