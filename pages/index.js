3edda763933e347b06614adf296eb97f348877a4
samsvk <samsvk00@gmail.com>
2022-06-10 00:20:41 +0100

    color schmecin

--- pages/index.js
+++ pages/index.js
@@ -36,11 +36,11 @@ export default function Home({ posts }) {
         {posts.map((post, index) => (
           <div key={index} className="flex flex-col items-center justify-center">
             <Link href={`${post}`}>
-              <div className="px-4 py-2.5 bg-gray-200 rounded-md hover:bg-gray-400 duration-300 hover:cursor-pointer">
+              <div className="px-4 py-2.5 rounded-md duration-300 hover:cursor-pointer">
                 <Image src="/folder.png" alt="folder picture" width="100px" height="100px" />
               </div>
             </Link>
-            <h1 className="m-0 mt-2 text-xs font-semibold text-gray-500">{post.replace(/^./, (str) => str.toUpperCase())}</h1>
+            <h1 className="m-0 mt-2 text-xs font-semibold text-zinc-50">{post.replace(/^./, (str) => str.toUpperCase())}</h1>
           </div>
         ))}
       </div>
--- pages/viper/index.js
+++ pages/viper/index.js
@@ -5,21 +5,31 @@ import Image from "next/image";
 import matter from "gray-matter";
 
 export default function Viper({ posts }) {
-  console.log(posts);
   return (
     <>
       <div className="flex items-center justify-center w-full max-w-3xl min-h-screen gap-3 p-10 mx-auto my-0">
         {posts.map((post, index) => {
-          const { date, title, image } = post.meta;
-          console.log(image);
+          const { date, title, image, post: link } = post.meta;
+
           return (
             <div key={index} className="flex flex-col items-center justify-center">
-              <Link href={`/viper/${title}`}>
-                <div className="px-4 py-2.5 bg-gray-200 rounded-md hover:bg-gray-400 duration-300 hover:cursor-pointer">
-                  <Image src={`${image}`} alt="folder picture" width="100px" height="100px" objectFit="cover" />
+              <Link href={`/viper/${link}`}>
+                <div className="relative block overflow-auto rounded-md drop-shadow-sm hover:cursor-pointer">
+                  <div className="absolute top-0 bottom-0 rounded-md  left-0 right-0 z-50 block w-full h-full bg-zinc-800 opacity-40 max-h-[145px]" />
+                  <Image
+                    quality={90}
+                    src={`${image}`}
+                    alt="folder picture"
+                    loading="eager"
+                    layout="fixed"
+                    width="235px"
+                    height="145px"
+                    objectFit="cover"
+                    className="block rounded-md"
+                  />
                 </div>
               </Link>
-              <h1 className="m-0 mt-2 text-xs font-semibold text-gray-500">{title}</h1>
+              <h1 className="m-0 mt-2 text-xs font-semibold text-zinc-500">{title}</h1>
             </div>
           );
         })}
