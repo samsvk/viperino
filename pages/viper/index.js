db4e430956c6eb27b64c44e97781b8a82c8e1fc4
samsvk <samsvk00@gmail.com>
2022-06-10 15:59:38 +0100

    ui update

--- pages/viper/index.js
+++ pages/viper/index.js
@@ -9,7 +9,7 @@ export default function Viper({ posts }) {
   return (
     <>
       {/* <div className="flex flex-wrap justify-start max-w-6xl m-0 mx-auto my-20 gap-y-12"> */}
-      <div className="grid flex-wrap justify-start max-w-6xl grid-cols-4 p-12 m-0 mx-auto my-20 gap-y-12">
+      <div className="grid flex-wrap justify-start max-w-6xl grid-cols-4 m-0 mx-auto my-20">
         {posts.map((post, index) => {
           const {
             date,
@@ -19,14 +19,13 @@ export default function Viper({ posts }) {
             tags,
             diff,
           } = post.meta;
-
           return (
             <div
               key={index}
-              className="flex flex-col self-start max-w-full max-h-max"
+              className="flex flex-col items-center self-center justify-center max-w-full overflow-hidden max-h-max"
             >
               <Link href={`/viper/${link}`}>
-                <div className="relative block overflow-auto duration-200 scale-105 rounded-md max-w-max shadow-inner-lg will-change-transform drop-shadow-sm hover:cursor-pointer">
+                <div className="relative block overflow-auto duration-200 rounded-md max-w-max shadow-inner-lg will-change-transform drop-shadow-sm hover:cursor-pointer">
                   <div
                     className="absolute top-0 bottom-0 rounded-md left-0 right-0 z-30 block w-full h-full
                    bg-zinc-900 opacity-40 max-h-[145px] max-w-full"
@@ -37,68 +36,74 @@ export default function Viper({ posts }) {
                   >
                     <IoPlayCircleOutline size={40} />
                   </div>
-                  <Image
-                    quality={90}
-                    src={`${image}`}
-                    alt="folder picture"
-                    loading="eager"
-                    layout="fixed"
-                    width="235px"
-                    height="145px"
-                    objectFit="cover"
-                    className="block rounded-md"
-                  />
+                  <div className="w-[245px] h-[135px] rounded-md block overflow-hidden">
+                    <Image
+                      quality={90}
+                      src={`${image}`}
+                      alt="folder picture"
+                      loading="eager"
+                      width="100%"
+                      height="100%"
+                      layout="fill"
+                      objectFit="cover"
+                      className="inline align-bottom scale-110 rounded-md line-height"
+                    />
+                  </div>
                 </div>
               </Link>
-              <div className="flex gap-3 mt-2">
-                <Image
-                  quality={100}
-                  src={`/viper.png`}
-                  alt="viper picture"
-                  loading="lazy"
-                  layout="fixed"
-                  width="28px"
-                  height="28px"
-                  objectFit="cover"
-                  className="block rounded-md"
-                />
-                <h1 className="mt-1 text-sm font-semibold tracking-wide text-left text-zinc-300">
-                  {title}
-                </h1>
+              <div className="w-full mt-2">
+                <div className="flex ml-6">
+                  <Image
+                    quality={100}
+                    src={`/viper.png`}
+                    alt="viper picture"
+                    loading="lazy"
+                    layout="fixed"
+                    width="40px"
+                    height="40px"
+                    objectFit="cover"
+                    className="block rounded-md leading-[0px]"
+                  />
+                  <div className="ml-3 mt-[-5px]">
+                    <h1 className="mt-1 text-sm font-semibold tracking-wide text-left text-zinc-300 ">
+                      {title}
+                    </h1>
+                    <ul className="flex w-full gap-2 mt-[-11px]">
+                      <li className="px-1.5 pt-.5 pb-0.5 rounded bg-zinc-800 flex justify-center items-center">
+                        <span
+                          className="inline-block w-1.5 h-1.5 mr-1.5 mt-0.5 rounded-full max-w-3"
+                          style={{
+                            background: `${
+                              diff == 1
+                                ? "#f87171"
+                                : diff === 2
+                                ? "#facc14"
+                                : "#49de80"
+                            }`,
+                          }}
+                        ></span>
+                        <span className="text-xs font-semibold text-zinc-400">
+                          {diff == 1
+                            ? "Hard"
+                            : diff === 2
+                            ? "Medium"
+                            : "Easy"}
+                        </span>
+                      </li>
+                      {tags.map((tag) => (
+                        <li
+                          key={tag}
+                          className="px-1.5 pt-.5 pb-0.5 rounded bg-zinc-800 flex justify-center items-center"
+                        >
+                          <span className="text-xs font-semibold text-zinc-400">
+                            {tag}
+                          </span>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
               </div>
-              <ul className="flex gap-3">
-                <li className="px-1.5 pt-.5 pb-0.5 rounded bg-zinc-800 flex justify-center items-center">
-                  <span
-                    className="inline-block w-1.5 h-1.5 mr-1.5 mt-0.5 rounded-full max-w-3"
-                    style={{
-                      background: `${
-                        diff == 1
-                          ? "#f87171"
-                          : diff === 2
-                          ? "#facc14"
-                          : "#49de80"
-                      }`,
-                    }}
-                  ></span>
-                  <span className="text-xs font-semibold text-zinc-400">
-                    {diff == 1
-                      ? "Hard"
-                      : diff === 2
-                      ? "Medium"
-                      : "Easy"}
-                  </span>
-                </li>
-                {tags.map((tag) => (
-                  <li
-                    key={tag}
-                    className="px-1.5 pt-.5 pb-0.5 rounded bg-zinc-800 flex justify-center items-center"
-                  >
-                    <span className="text-xs font-semibold text-zinc-400">
-                      {tag}
-                    </span>
-                  </li>
-                ))}
-              </ul>
             </div>
           );
         })}
