6a0213f8c6c910a38060b1b5d462e881c67db554
samsvk <samsvk00@gmail.com>
2022-06-10 23:33:48 +0100

    updated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["IBM Plex Sans", ...defaultTheme.fontFamily.sans],
+        fancy: ["IBM Plex Sans"],
+      },
+    },
+  },
   images: {
     domains: ["i.imgur.com"],
   },
