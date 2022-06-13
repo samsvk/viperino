4022e60c964741c6144c93e9cc13ef870a4d07dc
samsvk <samsvk00@gmail.com>
2022-06-13 01:53:09 +0100

    fully updated display

--- pages/viper/index.js
+++ pages/viper/index.js
@@ -13,10 +13,68 @@ import {
 export default function Viper({ posts }) {
   return (
     <>
-      <div className="max-w-[968px] p-5 w-100 mx-auto mt-20 text-[12px] bg-gray-100 flex items-center justify-center px-2 rounded gap-1">
-        123
+      <div className="max-w-[970px] py-5 px-0 w-100 mx-auto mt-20 text-[12px]flex rounded flex justify-between border-b border-gray-100">
+        <div className="max-w-max">
+          <h1 className="text-[14px] font-semibold tracking-wide text-gray-800">
+            Maps:
+          </h1>
+          <ul className="flex gap-2">
+            <li className="text-[12px] bg-gray-100 flex items-center justify-center max-w-fit px-2 rounded gap-1">
+              <span className="mt-[2px]">Breeze</span>
+            </li>
+            <li className="text-[12px] bg-gray-100 flex items-center justify-center max-w-fit px-2 rounded gap-1">
+              <span className="mt-[2px]">Bind</span>
+            </li>
+            <li className="text-[12px] bg-gray-100 flex items-center justify-center max-w-fit px-2 rounded gap-1">
+              <span className="mt-[2px]">Fracture</span>
+            </li>
+            <li className="text-[12px] bg-gray-100 flex items-center justify-center max-w-fit px-2 rounded gap-1">
+              <span className="mt-[2px]">Ascent</span>
+            </li>
+            <li className="text-[12px] bg-gray-100 flex items-center justify-center max-w-fit px-2 rounded gap-1">
+              <span className="mt-[2px]">Haven</span>
+            </li>
+            <li className="text-[12px] bg-gray-100 flex items-center justify-center max-w-fit px-2 rounded gap-1">
+              <span className="mt-[2px]">Icebox</span>
+            </li>
+            <li className="text-[12px] bg-gray-100 flex items-center justify-center max-w-fit px-2 rounded gap-1">
+              <span className="mt-[2px]">Split</span>
+            </li>
+          </ul>
+        </div>
+
+        <div className="max-w-max">
+          <h1 className="text-[14px] font-semibold tracking-wide text-gray-800">
+            Side:
+          </h1>
+          <ul className="flex gap-2">
+            <li className="text-[12px] bg-gray-100 flex items-center justify-center max-w-fit px-2 rounded gap-1">
+              <span className="mt-[2px]">Attack</span>
+            </li>
+            <li className="text-[12px] bg-gray-100 flex items-center justify-center max-w-fit px-2 rounded gap-1">
+              <span className="mt-[2px]">Defence</span>
+            </li>
+          </ul>
+        </div>
+
+        <div className="max-w-max">
+          <h1 className="text-[14px] font-semibold tracking-wide text-gray-800">
+            Difficulty:
+          </h1>
+          <ul className="flex gap-2">
+            <li className="text-[12px] bg-gray-100 flex items-center justify-center max-w-fit px-2 rounded gap-1">
+              <span className="mt-[2px]">Easy</span>
+            </li>
+            <li className="text-[12px] bg-gray-100 flex items-center justify-center max-w-fit px-2 rounded gap-1">
+              <span className="mt-[2px]">Medium</span>
+            </li>
+            <li className="text-[12px] bg-gray-100 flex items-center justify-center max-w-fit px-2 rounded gap-1">
+              <span className="mt-[2px]">Hard</span>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="gap-y-6 max-w-[990px] lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 w-100 grid mx-auto mt-8">
+      <div className="gap-y-6 max-w-[990px] lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 w-100 grid mx-auto mt-5">
         {posts.map((post, index) => {
           const {
             title,
