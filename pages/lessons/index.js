40603fba40407c1e22e7bbccddf13789b5e07aa7
samsvk <samsvk00@gmail.com>
2022-06-22 09:46:25 +0100

    working reset as well

--- pages/lessons/index.js
+++ pages/lessons/index.js
@@ -52,88 +52,118 @@ export default function Viper({ posts }) {
   return (
     <>
       <Maps handleRouter={handleRouter} />
-      <div className="gap-y-6 max-w-[990px] lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 w-100 grid mx-auto mt-5">
-        {filtered.map((post, index) => {
-          const { title, image, post: link, tags } = post.meta;
-          return (
-            <div
-              className="flex items-start justify-center"
-              key={index}
-            >
-              <div
-                key={index}
-                className="w-100% max-w-[305px] min-w-[245px] items-center justify-center rounded-md "
-              >
-                <Link href={`/lessons/${link}`}>
-                  <div className="relative block overflow-auto duration-200 rounded-md group drop-shadow-sm max-w-max shadow-inner-lg will-change-transform hover:cursor-pointer">
-                    <div
-                      className="absolute flex top-0 justify-center items-center bottom-0 rounded-t 
+      {filtered.length > 0 && (
+        <>
+          <div className="gap-y-6 max-w-[990px] lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 w-100 grid mx-auto mt-5">
+            {filtered.map((post, index) => {
+              const {
+                title,
+                image,
+                post: link,
+                tags,
+              } = post.meta;
+              return (
+                <div
+                  className="flex items-start justify-center"
+                  key={index}
+                >
+                  <div
+                    key={index}
+                    className="w-100% max-w-[305px] min-w-[245px] items-center justify-center rounded-md "
+                  >
+                    <Link href={`/lessons/${link}`}>
+                      <div className="relative block overflow-auto duration-200 rounded-md group drop-shadow-sm max-w-max shadow-inner-lg will-change-transform hover:cursor-pointer">
+                        <div
+                          className="absolute flex top-0 justify-center items-center bottom-0 rounded-t 
                   text-zinc-100 z-50 left-0 right-0 w-full h-full max-h-[168px]"
-                    >
-                      <IoPlayCircleOutline size={40} />
-                    </div>
-                    <div
-                      className="group-hover:opacity-5 duration-200 absolute top-0 bottom-0 rounded-t left-0 right-0 z-30 block w-full h-full
+                        >
+                          <IoPlayCircleOutline size={40} />
+                        </div>
+                        <div
+                          className="group-hover:opacity-5 duration-200 absolute top-0 bottom-0 rounded-t left-0 right-0 z-30 block w-full h-full
                    bg-zinc-900 opacity-[35%] max-h-[168px] max-w-full"
-                    />
-                    <div className="w-[305px] h-[168px] block overflow-hidden relative">
-                      <Image
-                        quality={90}
-                        src={`${image}`}
-                        alt="folder picture"
-                        loading="eager"
-                        layout="fill"
-                        objectFit="cover"
-                        className="inline shadow-inner align-bottom scale-[1.22] line-height"
-                      />
+                        />
+                        <div className="w-[305px] h-[168px] block overflow-hidden relative">
+                          <Image
+                            quality={90}
+                            src={`${image}`}
+                            alt="folder picture"
+                            loading="eager"
+                            layout="fill"
+                            objectFit="cover"
+                            className="inline shadow-inner align-bottom scale-[1.22] line-height"
+                          />
+                        </div>
+                      </div>
+                    </Link>
+                    <div className="mt-1.5 flex gap-2">
+                      <div className="max-h-35 min-h-35">
+                        <Image
+                          src={`/${link.split("_")[0]}.png`}
+                          height="35"
+                          width="35"
+                          quality={100}
+                          loading={"eager"}
+                        />
+                      </div>
+                      <div className="flex flex-col">
+                        <h1 className="text-[14px] font-semibold tracking-wide text-gray-800 leading-none">
+                          {title.split(" ").slice(1).join(" ")}
+                        </h1>
+                        <ul className="flex gap-2 mb-0 ml-0">
+                          <li className="text-[12px] bg-gray-100 flex items-center justify-center max-w-fit px-2 rounded gap-1">
+                            <IoStatsChart />
+                            <span className="mt-[2px]">
+                              {tags[0] == 1
+                                ? "Hard"
+                                : tags[0] === 2
+                                ? "Medium"
+                                : "Easy"}
+                            </span>
+                          </li>
+                          <li className="text-[12px] bg-gray-100 flex items-center justify-center max-w-fit px-2 rounded gap-1">
+                            <GiUnlitBomb />
+                            <span className="mt-[2px]">
+                              {tags[1]}
+                            </span>
+                          </li>
+                          <li className="text-[12px] bg-gray-100 flex items-center justify-center max-w-fit px-2 rounded gap-1">
+                            <IoLocation />
+                            <span className="mt-[2px]">
+                              {tags[2]}
+                            </span>
+                          </li>
+                        </ul>
+                      </div>
                     </div>
-                  </div>
-                </Link>
-                <div className="mt-1.5 flex gap-2">
-                  <div className="max-h-35 min-h-35">
-                    <Image
-                      src={`/${link.split("_")[0]}.png`}
-                      height="35"
-                      width="35"
-                      quality={100}
-                      loading={"eager"}
-                    />
-                  </div>
-                  <div className="flex flex-col">
-                    <h1 className="text-[14px] font-semibold tracking-wide text-gray-800 leading-none">
-                      {title.split(" ").slice(1).join(" ")}
-                    </h1>
-                    <ul className="flex gap-2 mb-0 ml-0">
-                      <li className="text-[12px] bg-gray-100 flex items-center justify-center max-w-fit px-2 rounded gap-1">
-                        <IoStatsChart />
-                        <span className="mt-[2px]">
-                          {tags[0] == 1
-                            ? "Hard"
-                            : tags[0] === 2
-                            ? "Medium"
-                            : "Easy"}
-                        </span>
-                      </li>
-                      <li className="text-[12px] bg-gray-100 flex items-center justify-center max-w-fit px-2 rounded gap-1">
-                        <GiUnlitBomb />
-                        <span className="mt-[2px]">
-                          {tags[1]}
-                        </span>
-                      </li>
-                      <li className="text-[12px] bg-gray-100 flex items-center justify-center max-w-fit px-2 rounded gap-1">
-                        <IoLocation />
-                        <span className="mt-[2px]">
-                          {tags[2]}
-                        </span>
-                      </li>
-                    </ul>
                   </div>
                 </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+              );
+            })}
+          </div>
+        </>
+      )}
+      {filtered.length === 0 && (
+        <div className="max-w-[440px] py-3 px-0 w-100 mx-auto text-[12px]flex rounded flex justify-center border-gray-100 items-center mt-10">
+          <div className="p-5 border border-gray-100 rounded">
+            <p>
+              Unfortunately, we couldn't find any posts with
+              these filters... We're working hard on producing
+              more guides maybe check back later.
+            </p>
+            <ul className="flex gap-2 ml-0">
+              <li
+                onClick={() => Router.push("/lessons")}
+                className="text-[12px] bg-gray-100 flex items-center justify-center max-w-fit px-2 rounded gap-1  hover:cursor-pointer hover:bg-slate-200 hover:text-slate-50 duration-200"
+              >
+                <span className="mt-[2px]">
+                  Reset Filters...
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
     </>
   );
 }
