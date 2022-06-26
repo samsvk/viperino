24affb5923c6ee869530c5a4e6967ebd6f5393f4
samsvk <samsvk00@gmail.com>
2022-06-26 23:11:27 +0100

    updated topbar

--- components/heading.js
+++ components/heading.js
@@ -2,9 +2,11 @@ import React from "react";
 
 const Heading = (props) => {
   return (
-    <div className="max-w-[970px] py-5 px-0 w-100 mx-auto text-[12px]flex rounded flex justify-between border-b border-gray-100 items-center mt-10">
-      <div className="min-h-[65px] min-w-[65px] bg-gray-100 rounded-full"></div>
-      Valorant Setup/Lineup Guides.
+    <div className="max-w-[970px] py-5 px-0 w-100 mx-auto text-[12px] rounded border-b border-gray-100 items-center mt-10 flex">
+      <div className="min-h-[65px] max-h-[65px] min-w-[65px] max-w-[65px] bg-gray-100 rounded-full block"></div>
+      <div>
+        <h1 className="block">{props.title}</h1>
+      </div>
     </div>
   );
 };
