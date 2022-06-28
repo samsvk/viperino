7c93201430929d004f47bc3df9d5201e4d5c281f
samsvk <samsvk00@gmail.com>
2022-06-28 14:00:12 +0100

    updated ui

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,66 @@
 import * as React from "react";
 import Image from "next/image";
 import { BsArrowRight } from "react-icons/bs";
-
+import { AiOutlineDollarCircle } from "react-icons/ai";
+import { MdAutoGraph } from "react-icons/md";
+import { TbNumbers, TbArrowsRandom } from "react-icons/tb";
 export default function Home({ posts }) {
   return (
     <>
       <div className="flex items-center w-full h-full min-h-screen bg-black/80 min-w-screen">
-        <div className="flex w-full max-w-4xl gap-8 mx-auto">
-          <div className="flex flex-1"></div>
+        <div className="flex w-full max-w-4xl gap-5 mx-auto">
+          <div className="grid max-w-[385px] w-full grid-cols-2 grid-rows-2 gap-4">
+            <div className="flex flex-col items-center justify-center gap-1.5 px-3 border border-green-900 rounded-md bg-green-900/10">
+              <h4 className="text-[28px] text-green-500 ">
+                <AiOutlineDollarCircle />
+              </h4>
+              <h5 className="text-base font-semibold text-white">
+                Value For Money
+              </h5>
+              <p className="text-center text-[12px] text-white/60 leading-4 pb-0 mb-0">
+                We produce the best guides for less. No
+                Subscription, No Contract.
+              </p>
+            </div>
+            <div className="flex flex-col items-center justify-center gap-1.5 px-3 border rounded-md border-fuchsia-900 bg-fuchsia-900/10">
+              <h4 className="text-[28px] text-fuchsia-500 ">
+                <TbNumbers />
+              </h4>
+              <h5 className="text-base font-semibold text-white">
+                Unlimited Guides
+              </h5>
+              <p className="text-center text-[12px] text-white/60 leading-4 pb-0 mb-0">
+                Permanent access to handcrafted guides from top
+                ranked players available at your fingertips.
+              </p>
+            </div>
+            <div className="flex flex-col items-center justify-center gap-1.5 px-3 border rounded-md border-cyan-900 bg-cyan-900/10">
+              <h4 className="text-[28px] text-cyan-500 ">
+                <TbArrowsRandom />
+              </h4>
+              <h5 className="text-base font-semibold text-white">
+                All Agents & Maps
+              </h5>
+              <p className="text-center text-[12px] text-white/60 leading-4 pb-0 mb-0">
+                We focus on fundamental game mechanics that
+                translate to all agents.
+              </p>
+            </div>
+            <div className="flex flex-col items-center justify-center gap-1.5 px-3 border border-indigo-900 rounded-md bg-indigo-900/10">
+              <h4 className="text-[28px] text-indigo-500 ">
+                <MdAutoGraph />
+              </h4>
+              <h5 className="text-base font-semibold text-white">
+                Progress Over Time
+              </h5>
+              <p className="text-center text-[12px] text-white/60 leading-4 pb-0 mb-0">
+                Our guides upscale - Iron to Radiant
+                there&lsquo;s something for everyone.
+              </p>
+            </div>
+          </div>
           <div className="flex flex-col items-center justify-center flex-1 px-3 ">
-            <div className="mx-auto mb-2 relative min-h-[88px] overflow-hidden min-w-[88px] max-h-[88px] max-w-[88px] w-full h-full rounded-xl ">
+            <div className="mx-auto mb-2 relative min-h-[88px] overflow-hidden min-w-[88px] max-h-[88px] max-w-[88px] w-full h-full rounded-xl">
               <Image
                 src="/radiant.svg"
                 alt="folder picture"
@@ -37,9 +88,9 @@ export default function Home({ posts }) {
             </div>
             <div className="max-w-[400px] mt-10 bg-black/5 duration-300 border rounded-lg border-neutral-900 relative overflow-hidden">
               <p className="px-5 pt-4 pb-4 mb-0 text-center">
-                No Subscription - One Time Payment: for the price
-                of one pizza we&lsquo;ll elevate your game to the
-                next level.
+                One Time Payment - for the price of one in-game
+                skin we&lsquo;ll elevate your game to the next
+                level.
               </p>
               <button className="group hover:bg-white/5 duration-300 w-full py-2 m-0 border-t border-neutral-900 text-[14px] tracking-wide font-light text-white/90 leading-5 flex items-center justify-center">
                 Continue to Payment{" "}
