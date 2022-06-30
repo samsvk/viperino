322e62e7802091068f75f702466ac3e118eb88d3
samsvk <samsvk00@gmail.com>
2022-06-30 13:02:37 +0100

    upda

--- components/footer.js
+++ components/footer.js
@@ -2,9 +2,13 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <div className="max-w-[1100px] w-full mx-auto py-20">
-      <p className="text-[16px] leading-6 font-normal text-neutral-700/60 my-10">
-        123
+    <div className="max-w-[875px] w-full mx-auto p-12 mt-20 rounded-lg flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-semibold tracking-[0] text-black">
+        Our Community and Contributers.
+      </h1>
+      <p className="text-[20px] leading-7 font-normal text-neutral-700/60 my-5 text-center">
+        We are determined to bring meaningful change to
+        educational gaming and provide an ecosystem for all.
       </p>
     </div>
   );
