d40cad0a174c5cdfc9aec670d0b7509cec70312c
samsvk <samsvk00@gmail.com>
2022-07-01 18:45:36 +0100

    updated design

--- components/footer.js
+++ components/footer.js
@@ -2,36 +2,38 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <div className="max-w-[875px] w-full mx-auto p-12 pb-[8rem] mt-20 rounded-lg flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-semibold tracking-[0] text-black">
-        Our Community and Contributers.
-      </h1>
-      <p className="text-[22px] leading-7 font-normal text-neutral-700/60 my-5 text-center">
-        We are determined to bring change to educational gaming
-        and provide an ecosystem for all - together we
-        collaberate to build one tool for you.
-      </p>
+    <>
+      <div className="max-w-[800px] w-full mx-auto p-12 pb-[8rem] mt-20 rounded-lg flex flex-col items-center justify-center relative">
+        <h1 className="text-4xl font-semibold tracking-[0] text-black">
+          Our Community and Contributers.
+        </h1>
+        <p className="text-[18px] leading-7 font-normal text-neutral-700/60 my-8 text-center">
+          We are determined to bring change to educational gaming
+          and provide an ecosystem for all - together we
+          collaberate to build one tool for you.
+        </p>
 
-      <div className="flex gap-3 mt-1">
-        <button className="flex flex-row items-center justify-center gap-1.5 py-5 px-8 border border-gray rounded-md overflow-hidden relative hover:bg-black/5 duration-100">
-          <span>🐦</span>
-          <h5 className="text-base font-medium text-black/80">
-            Twitter
-          </h5>
-        </button>
-        <button className="flex flex-row items-center justify-center gap-1.5 py-5 px-8 border border-gray rounded-md overflow-hidden relative hover:bg-black/5 duration-100">
-          <span> 🔴</span>
-          <h5 className="text-base font-medium text-black/80">
-            YouTube
-          </h5>
-        </button>
-        <button className="flex flex-row items-center justify-center gap-1.5 py-5 px-8 border border-gray rounded-md overflow-hidden relative hover:bg-black/5 duration-100">
-          <span>🟣</span>
-          <h5 className="text-base font-medium text-black/80">
-            Discord
-          </h5>
-        </button>
+        <div className="flex gap-3 mt-1">
+          <button className="flex flex-row items-center justify-center gap-1.5 py-5 px-8 border border-gray rounded-md overflow-hidden relative hover:bg-black/5 duration-100">
+            <span>🐦</span>
+            <h5 className="text-base font-medium text-black/80">
+              Twitter
+            </h5>
+          </button>
+          <button className="flex flex-row items-center justify-center gap-1.5 py-5 px-8 border border-gray rounded-md overflow-hidden relative hover:bg-black/5 duration-100">
+            <span> 🔴</span>
+            <h5 className="text-base font-medium text-black/80">
+              YouTube
+            </h5>
+          </button>
+          <button className="flex flex-row items-center justify-center gap-1.5 py-5 px-8 border border-gray rounded-md overflow-hidden relative hover:bg-black/5 duration-100">
+            <span>🟣</span>
+            <h5 className="text-base font-medium text-black/80">
+              Discord
+            </h5>
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -11,15 +11,15 @@ export default function Home({ posts }) {
   return (
     <>
       <div className="flex flex-col">
-        <div className="max-w-[1100px] w-full mx-auto pt-40">
-          <div className="max-w-[550px] relative z-50">
-            <h1 className="text-5xl font-semibold tracking-[0] text-black">
+        <div className="max-w-[1200px] w-full mx-auto pt-40">
+          <div className="max-w-[600px] relative z-50">
+            <h1 className="text-6xl font-semibold tracking-[0] text-black">
               Become a member.
             </h1>
-            <h1 className="text-5xl font-semibold tracking-[0] text-black">
+            <h1 className="text-6xl font-semibold tracking-[0] text-black">
               Gain access now.
             </h1>
-            <p className="text-[16px] leading-6 font-normal text-neutral-700/60 my-10">
+            <p className="text-[18px] leading-7 font-normal text-neutral-700/60 my-10">
               Support Viperino development and gain access to new
               tools and ideas that will help push you past the
               Valorant learning curve.
@@ -27,12 +27,14 @@ export default function Home({ posts }) {
 
             <button
               className="px-5 py-1.5 bg-black rounded-full text-[14px] leading-6 font-medium text-white hover:cursor-pointer 
-              hover:bg-black/80 duration-200"
+              hover:bg-black/80 duration-200
+              
+              "
             >
               Join Viperino Now
             </button>
 
-            <p className="text-[13px] leading-5 font-medium text-neutral-700/80 my-10">
+            <p className="text-[14px] leading-5 font-normal text-neutral-700/80 my-10">
               <span className="underline">
                 Limited price for New Members:
               </span>{" "}
@@ -47,9 +49,9 @@ export default function Home({ posts }) {
       </div>
 
       <div>
-        <div className="flex items-center w-full gap-5 mx-auto max-w-[1100px]  rounded-lg mb-10 mt-20">
+        <div className="flex items-center w-full gap-5 mx-auto max-w-[1200px]  rounded-lg mb-10 mt-20">
           <div className="grid max-w-[100%] w-full grid-cols-3 grid-rows-2 gap-10">
-            <div className="flex flex-col items-center justify-center gap-1.5 py-5 px-8 border border-gray rounded-md overflow-hidden relative">
+            <div className="flex py-10 flex-col items-center justify-center gap-1.5 px-8 border border-gray rounded-md overflow-hidden relative">
               <h4 className="text-[28px] text-neutral-700/10">
                 <AiOutlineDollarCircle />
               </h4>
@@ -57,7 +59,7 @@ export default function Home({ posts }) {
                 Value For Money
               </h5>
 
-              <p className="text-[13px] text-center leading-5 font-medium text-neutral-700/80 m-0 p-0">
+              <p className="text-[14px] text-center leading-5 font-normal text-neutral-700/80 m-0 p-0">
                 We produce the best guides for less. No
                 Subscription, No Contract - Simply a One-Time
                 payment.
@@ -71,7 +73,7 @@ export default function Home({ posts }) {
               <h5 className="text-base font-medium text-black/80">
                 Unlimited Guides
               </h5>
-              <p className="text-[13px] text-center leading-5 font-medium text-neutral-700/80 m-0 p-0">
+              <p className="text-[14px] text-center leading-5 font-normal text-neutral-700/80 m-0 p-0">
                 Permanent access to handcrafted guides from top
                 ranked players available at your fingertips.
               </p>
@@ -83,7 +85,7 @@ export default function Home({ posts }) {
               <h5 className="text-base font-medium text-black/80">
                 All Agents - All Maps
               </h5>
-              <p className="text-[13px] text-center leading-5 font-medium text-neutral-700/80 m-0 p-0">
+              <p className="text-[14px] text-center leading-5 font-normal text-neutral-700/80 m-0 p-0">
                 We cover fundamental game mechanics that
                 translate to all agents.
               </p>
@@ -95,7 +97,7 @@ export default function Home({ posts }) {
               <h5 className="text-base font-medium text-black/80">
                 Progress Over Time
               </h5>
-              <p className="text-[13px] text-center leading-5 font-medium text-neutral-700/80 m-0 p-0">
+              <p className="text-[14px] text-center leading-5 font-normal text-neutral-700/80 m-0 p-0">
                 Our guides upscale - Iron to Radiant
                 there&lsquo;s something for everyone.
               </p>
@@ -108,7 +110,7 @@ export default function Home({ posts }) {
               <h5 className="text-base font-medium text-black/80">
                 Speed
               </h5>
-              <p className="text-[13px] text-center leading-5 font-medium text-neutral-700/80 m-0 p-0">
+              <p className="text-[14px] text-center leading-5 font-normal text-neutral-700/80 m-0 p-0">
                 New Update? No problem - we guarantee to have
                 content produced or updated intime for
                 competitive release.
@@ -122,7 +124,7 @@ export default function Home({ posts }) {
               <h5 className="text-base font-medium text-black/80">
                 Quality
               </h5>
-              <p className="text-[13px] text-center leading-5 font-medium text-neutral-700/80 m-0 p-0">
+              <p className="text-[14px] text-center leading-5 font-normal text-neutral-700/80 m-0 p-0">
                 From our entire system, to our content produced -
                 we have build a foundation that allows us to
                 produce the highest quality guides.
