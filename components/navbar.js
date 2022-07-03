856552de44fd7b0fc814203975c4219331ae831d
samsvk <samsvk00@gmail.com>
2022-07-03 11:29:17 +0100

    working mobile nav

--- components/navbar.js
+++ components/navbar.js
@@ -22,37 +22,48 @@ export function Navbar() {
   }, []);
 
   return (
-    <div
-      className={`max-w-[100%] px-6 flex z-[1000] w-full mx-auto fixed left-0 right-0 bottom-0 max-h-[50px]
-      justify-start items-start duration-200  ${
+    <>
+      <div
+        className={`max-w-[100%] px-6 z-[1000] w-full mx-auto fixed left-0 right-0 bottom-0 max-h-[50px]
+      justify-start items-start duration-200 hidden md:block ${
         isAtTop
           ? "bg-transparent border-transparent top-8"
           : "backdrop-blur border-b border-gray top-[0rem]"
       }
-  `}
-    >
-      <nav className="max-w-[1155px] mx-auto w-full flex items-center">
-        <div className="min-h-[40px] min-w-[40px] w-full h-full max-w-[40px] max-h-[40px] bg-black rounded-full"></div>
-        <ul className="flex gap-5 p-0 m-0 list-none  py-3.5 ml-10 flex-1">
-          <li className="p-0 m-0 text-base font-normal tracking-tight duration-200 text-neutral-700/60 hover:cursor-pointer hover:text-black/80">
-            Features
-          </li>
-          <li className="p-0 m-0 text-base font-normal tracking-tight text-neutral-700/60 hover:cursor-pointer hover:text-black/80">
-            Community
-          </li>
-          <li className="p-0 m-0 text-base font-normal tracking-tight text-neutral-700/60 hover:cursor-pointer hover:text-black/80">
-            Contact
-          </li>
-        </ul>
-        <div className="flex items-center justify-center h-full flex-2 mb-[5px]">
-          <button
-            className="px-6 py-2 bg-black rounded-full text-[14px] leading-6 font-normal text-white hover:cursor-pointer 
-              hover:bg-black/80 duration-200"
-          >
-            Login Now
-          </button>
-        </div>
-      </nav>
-    </div>
+      `}
+      >
+        <nav className="max-w-[1155px] mx-auto w-full flex items-center">
+          <div className="min-h-[40px] min-w-[40px] w-full h-full max-w-[40px] max-h-[40px] bg-black rounded-full"></div>
+          <ul className="flex gap-5 p-0 m-0 list-none  py-3.5 ml-10 flex-1">
+            <li className="p-0 m-0 text-base font-normal tracking-tight duration-200 text-neutral-700/60 hover:cursor-pointer hover:text-black/80">
+              Features
+            </li>
+            <li className="p-0 m-0 text-base font-normal tracking-tight text-neutral-700/60 hover:cursor-pointer hover:text-black/80">
+              Community
+            </li>
+            <li className="p-0 m-0 text-base font-normal tracking-tight text-neutral-700/60 hover:cursor-pointer hover:text-black/80">
+              Contact
+            </li>
+          </ul>
+          <div className="flex items-center justify-center h-full flex-2 mb-[5px]">
+            <button
+              className="px-6 py-2 bg-black rounded-full text-[14px] leading-6 font-normal text-white hover:cursor-pointer 
+            hover:bg-black/80 duration-200"
+            >
+              Login Now
+            </button>
+          </div>
+        </nav>
+      </div>
+
+      <div
+        className={`max-w-[100%] px-6 z-[1000] w-full mx-auto fixed left-0 right-0 bottom-0 max-h-[50px]
+      justify-start items-start duration-200 backdrop-blur border-b border-gray top-[0rem] block md:hidden lg:hidden"
+    
+      `}
+      >
+        123 123123
+      </div>
+    </>
   );
 }
