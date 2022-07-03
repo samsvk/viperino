50c56d8465d535cd0a3ce623c50e65585b5a31ad
samsvk <samsvk00@gmail.com>
2022-07-03 12:24:04 +0100

    updated menu

--- components/navbar.js
+++ components/navbar.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-import { HiMenu } from "react-icons/hi";
+import { HiMenu, HiLogin } from "react-icons/hi";
+import { SiDiscord } from "react-icons/si";
 
 export function getWindow() {
   return typeof window !== "undefined" ? window : null;
@@ -38,13 +39,13 @@ export function Navbar() {
         <nav className="max-w-[1155px] mx-auto w-full flex items-center">
           <div className="min-h-[40px] min-w-[40px] w-full h-full max-w-[40px] max-h-[40px] bg-black rounded-full"></div>
           <ul className="flex gap-5 p-0 m-0 list-none  py-3.5 ml-10 flex-1">
-            <li className="p-0 m-0 text-base font-normal tracking-tight duration-200 text-neutral-700/60 hover:cursor-pointer hover:text-black/80">
+            <li className="text-[14px] leading-5 font-normal text-neutral-700/80 m-0 p-0 tracking-tight">
               Features
             </li>
-            <li className="p-0 m-0 text-base font-normal tracking-tight text-neutral-700/60 hover:cursor-pointer hover:text-black/80">
+            <li className="text-[14px] leading-5 font-normal text-neutral-700/80 m-0 p-0 tracking-tight">
               Community
             </li>
-            <li className="p-0 m-0 text-base font-normal tracking-tight text-neutral-700/60 hover:cursor-pointer hover:text-black/80">
+            <li className="text-[14px] leading-5 font-normal text-neutral-700/80 m-0 p-0 tracking-tight">
               Contact
             </li>
           </ul>
@@ -66,16 +67,45 @@ export function Navbar() {
       >
         <div className="min-h-[40px] min-w-[40px] w-full h-full max-w-[40px] max-h-[40px] bg-black rounded-full relative" />
         <div className="flex items-end justify-end flex-1">
-          <div className="px-1 py-0.5 flex items-center justify-center border border-transparent rounded-md hover:cursor-pointer hover:border-black/5 duration-150 hover:bg-black/5 text-neutral-700/60">
-            <HiMenu
-              size={25}
-              onClick={() => setShow((p) => !p)}
-            />
-          </div>
+          <button
+            onClick={() => setShow((p) => !p)}
+            className={`px-1 py-0.5 flex items-center justify-center border border-transparent rounded-md hover:cursor-pointer hover:border-black/5 duration-150 hover:bg-black/5 text-neutral-700/60
+              ${show && "bg-black/5 border-black/5"}
+            `}
+          >
+            <HiMenu size={25} />
+          </button>
         </div>
 
         {show && (
-          <div className="absolute right-5 top-[5rem]">123</div>
+          <div className="absolute right-5 top-[4rem]">
+            <div className="z-10 flex flex-row items-center justify-center py-1 min-w-[165px] gap-1.5 border border-gray rounded-md overflow-hidden relative bg-black/5 duration-100">
+              <ul className="flex flex-col flex-1 gap-1 px-1 py-0 m-0 list-none">
+                <li className="flex items-center gap-2 duration-150 hover:bg-black/10 hover:cursor-pointer px-3 text-[14px] leading-5 font-normal text-neutral-700/80 m-0 p-0 tracking-tight py-[3px] rounded-md">
+                  <span>
+                    <HiLogin size={18} />
+                  </span>
+                  Login Now
+                </li>
+                <li className="flex items-center gap-2 duration-150 hover:bg-black/10 hover:cursor-pointer px-3 text-[14px] leading-5 font-normal text-neutral-700/80 m-0 p-0 tracking-tight py-[3px] rounded-md">
+                  <span>
+                    <SiDiscord size={16} />
+                  </span>
+                  Join Discord
+                </li>
+                <hr className="block h-[1px] bg-black/5 border-0 my-0.5" />
+                <li className=" duration-150 hover:bg-black/10 hover:cursor-pointer px-3 text-[14px] leading-5 font-normal text-neutral-700/80 m-0 p-0 tracking-tight py-[3px] rounded-md">
+                  Features
+                </li>
+                <li className="duration-150 hover:bg-black/10 hover:cursor-pointer px-3 text-[14px] leading-5 font-normal text-neutral-700/80 m-0 p-0 tracking-tight py-[3px] rounded-md">
+                  Community
+                </li>
+                <li className="duration-150 hover:bg-black/10 hover:cursor-pointer px-3 text-[14px] leading-5 font-normal text-neutral-700/80 m-0 p-0 tracking-tight py-[3px] rounded-md">
+                  Contact
+                </li>
+              </ul>
+            </div>
+          </div>
         )}
       </div>
     </>
