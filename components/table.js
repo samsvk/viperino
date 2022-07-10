884b667297d707156af721dd36b213ccbd64bb9a
samsvk <samsvk00@gmail.com>
2022-07-10 01:50:26 +0100

    updated post

--- components/table.js
+++ components/table.js
@@ -62,7 +62,7 @@ export const Table = ({ post }) => {
             tracking-normal no-underline text-[14px] font-medium text-left duration-75  text-black/80 relative
             flex gap-1"
         >
-          <span>Admin /</span>
+          <span>Posted: </span>
           <span>{post.meta.date.split(" ")[1]}</span>
           <span>{post.meta.date.split(" ")[2]},</span>
           <span>{post.meta.date.split(" ")[3]}</span>
